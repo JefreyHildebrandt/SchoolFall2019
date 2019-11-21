df10e5d85e10683179e83033a36c5fa898426d93
JefreyHildebrandt <JefreyHildebrandt@gmail.com>
2019-11-20 20:46:39 -0500

    Finished PageRank

--- COP5537 - Network Optimization/Assignment 3/PageRank.py
+++ COP5537 - Network Optimization/Assignment 3/PageRank.py
@@ -29,27 +29,91 @@ class Network:
         weighted_adjacency_array = line.split('\t')[1].split(',')
         return [float(x) for x in weighted_adjacency_array]
 
+class PageRank:
+    # class PageRank:
+    @staticmethod
+    def page_rank(graph, dampening, convergence):
+        total_pages = len(graph)
+        # this will be added to the final value of each page_rank at the end of an iteration
+        dampening_base = (1-dampening)/total_pages
+        # initialize ranks with default value of 1/N
+        page_ranks = [1/total_pages] * total_pages
+        previous_page_ranks = [-1] * total_pages
+        # the number of outgoing links that each page has
+        outgoing_links = [0] * total_pages
+        for i in range(len(graph)):
+            outgoing_links[i] = PageRank.count_links(graph[i])
+
+        iterations = 0
+        while not PageRank.has_converged(previous_page_ranks, page_ranks, convergence):
+            previous_page_ranks = page_ranks
+            page_ranks = [0] * total_pages
+
+            for i in range(len(graph)):
+                num_links_for_page = outgoing_links[i]
+                self_page_rank = previous_page_ranks[i] / num_links_for_page
+
+                for j in range(len(graph[i])):
+                    connected = graph[i][j]
+                    if not connected:
+                        continue
+                    # add the page_rank value to any connecting nodes
+                    page_ranks[j] = page_ranks[j] + self_page_rank
+
+            # multiply by the dampening and add the dampening_base to each page as per the formula
+            for i in range(len(page_ranks)):
+                page_ranks[i] = page_ranks[i] * dampening
+                page_ranks[i] = page_ranks[i] + dampening_base
+            iterations = iterations + 1
+
+        return page_ranks, iterations
+
+    @staticmethod
+    def count_links(page_links):
+        link_count = 0
+        #finds the number of links a page has
+        for connected in page_links:
+            if connected:
+                link_count = link_count + 1
+        return link_count
+
+    @staticmethod
+    def has_converged(previous_page_ranks, page_ranks, convergence):
+        # ensures that every value in page_ranks is less than the convergence value
+        for i in range(len(previous_page_ranks)):
+            difference = abs(page_ranks[i] - previous_page_ranks[i])
+            if difference > convergence:
+                return False
+        return True
+
+    @staticmethod
+    def save_results(page_ranks, iterations):
+        print_results = 'number of iterations:' + str(iterations) + '\n'
+        tuple_array = [(i,page_ranks[i]) for i in range(len(page_ranks))]
+        sorted_tuple_array = sorted(tuple_array, key=lambda rank: rank[1], reverse=True)
+        print_results = print_results + 'ranking:'
+        for node, result in sorted_tuple_array:
+            print_results = print_results + str(node) + '(' + str(result) + '),'
+        print_results = print_results[:-1]
+
+        with open('output.txt', 'w') as f:
+            f.write(print_results)
+
+        print(print_results)
+
 def main():
     if len(sys.argv) < 2:
-        print('Please pass a filename then pid as a parameter.')
+        print('Please pass a filename then dampening as a parameter.')
         return
 
     network_file = sys.argv[1]
-    pid = None
-    if(len(sys.argv) > 2):
-        pid = sys.argv[2]
 
-    starting_vertex = int(pid[-2:])
+    dampening = float(sys.argv[2])
+    convergence = 1.0e-5
 
     network = Network(network_file)
-
-    # output = Fleury.fleurys_algorithm(network.weighted_adjacency_matrix, starting_vertex)
-    # formatted_output = ', '.join(output)
-    # print(formatted_output)
-    # with open('output.txt', 'w') as f:
-    #     f.write(formatted_output)
-
-
+    page_ranks, iterations = PageRank.page_rank(network.weighted_adjacency_matrix, dampening, convergence)
+    PageRank.save_results(page_ranks, iterations)
 
 if __name__== "__main__":
   main()
